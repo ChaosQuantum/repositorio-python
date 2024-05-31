e60718144cd2b53ac9f412518b741a2574bf26aa
caqr <caoslml@gmail.com>
2024-05-31 18:31:00 -0500

    segundo algoritmo de python

---
+++ 2_5.py
@@ -0,0 +1,10 @@
+# Una empresa constructora vende terrenos con la forma A de la figura
+# Descripcion de la figura un triangulo y un rectangulo unidos
+
+valor_metro2=2500
+
+metro2=int(input("Ingrese el valor de los metros cuadrados a trabajar "))
+
+costo=valor_metro2 * metro2
+print(f"El valor total del trabajo es de: {costo}
+")
