e38f598cd04db6876221632d82082f580ea70149
caqr <caoslml@gmail.com>
2024-05-31 18:26:44 -0500

    primer algoritmo en python

---
+++ 2_1.py
@@ -0,0 +1,8 @@
+#Se desea implementar un algoritmo para obtener la suma de dos números 
+# cualesquiera.
+
+año_actual= 2024
+año_nacimiento=int(input("Ingrese el año de nacimiento "))
+
+edad=año_actual-año_nacimiento
+print(edad)
